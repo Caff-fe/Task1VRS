f3633d934d225ba5c4aabb62f969c1ab71843686
Dimo Yordanov <dimoy73@gmail.com>
2024-06-16 22:14:13 +0300

    added missing sercurity rating in car class

--- src/Vehicle/Car.java
+++ src/Vehicle/Car.java
@@ -1,22 +1,37 @@
 package Vehicle;
 
 public class Car extends Vehicle {
-    private double insuranceCost = 0.01;
+    private double insuranceCost = 0.0001;
+    private double dailyRental = 20.00;
+    private int sercurityRating;
 
-    public Car(String brand, double value) {
+    public Car(String brand, double value, int sercurityRating) {
         super(brand, value);
+        this.sercurityRating = sercurityRating;
     }
 
     public double getInsuranceCost() {
         return insuranceCost;
     }
 
+    public double getDailyRental() {
+        return dailyRental;
+    }
+
+    public int getSecurityRating() {
+        return sercurityRating;
+    }
+
+    public void setDailyRental(double dailyRental) {
+        this.dailyRental = dailyRental;
+    }
+
     @Override
     public String toString() {
         return "Car{" +
-                "brand='" + getBrand() + '\'' +
-                ", value=" + getValue() +
-                ", insuranceCost=" + insuranceCost +
+                "insuranceCost=" + insuranceCost +
+                ", dailyRental=" + dailyRental +
+                ", sercurityRating=" + sercurityRating +
                 '}';
     }
 }
